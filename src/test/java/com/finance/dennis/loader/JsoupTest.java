d13177bdc38efbde9685db6ae9ec5fe31a6ab0aa
chinamonkeyking <chinamonkeyking@gmail.com>
2016-11-15 00:28:11 +0800

    Add more test

--- src/test/java/com/finance/dennis/loader/JsoupTest.java
+++ src/test/java/com/finance/dennis/loader/JsoupTest.java
@@ -5,26 +5,28 @@
 
 package com.finance.dennis.loader;
 
+import jdk.nashorn.internal.runtime.regexp.joni.encoding.IntHolder;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by XiaoNiuniu on 11/12/2016.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring/domain-context.xml", "classpath:spring/hsqldb-datasource.xml", "classpath:spring/jpa.xml"})
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:spring/domain-context.xml", "classpath:spring/hsqldb-datasource.xml", "classpath:spring/jpa.xml"})
 public class JsoupTest {
 
     private static final String CHARSET_NAME = "GB2312";
@@ -58,7 +60,7 @@ public class JsoupTest {
                 if (!ALL_OPEN_FUNDS_TYPE.equals(element.attr(ATTR_FUND_TYPE))) {
                     types.add(element.attr(ATTR_FUND_TYPE));
                     names.add(element.text());
-                    System.out.println(element.text());
+                    //System.out.println(element.text());
                 }
             }
 
@@ -74,46 +76,86 @@ public class JsoupTest {
     }
 
     @Test
-    public void testFundPage() {
+    public void testSubTypePage() {
 
-/*        InputStream in = JsoupTest.class.getClassLoader().getResourceAsStream("files\\jingzhi_main_page.html");
+        InputStream in = JsoupTest.class.getClassLoader().getResourceAsStream("files\\jingzhi_subtype.html");
+
         try {
-            Document doc = Jsoup.parse(in, CHARSET_NAME, DEFAULT_BLANK_BASE_URL);
-            Elements elements = doc.select("div[class=fundSecNav]>a");
-            Assert.assertEquals(14, elements.size());
-
-            ArrayList<String> types = new ArrayList<String>();
-            ArrayList<String> names = new ArrayList<String>();
-            for (Element element : elements) {
-                // Skip the type for all funds
-                if (!ALL_OPEN_FUNDS_TYPE.equals(element.attr(ATTR_FUND_TYPE))) {
-                    types.add(element.attr(ATTR_FUND_TYPE));
-                    names.add(element.text());
-                    System.out.println(element.text());
-                }
+            String fileContent = IOUtils.toString(in, "GBK");
+            Pattern p= Pattern.compile("callback\\((.*)\\)");
+            Matcher m= p.matcher(fileContent);
+            String fundContent = "{}";
+            if(m.find()) {
+                fundContent = m.group(1);
             }
 
-            String[] expectedTypes = {"2", "3", "4", "6", "8", "9", "7", "11", "10", "12", "5", "40", "70"};
-            String[] expectedNames = {"股票型", "配置型", "债券型", "保本型", "ETF", "LOF", "QDII", "FOF", "联接", "创新型", "指数型", "货币型", "创新理财型"};
+            JSONObject funds = new JSONObject(fundContent);
+            int fundCnt = funds.getInt("sum");
+            Assert.assertEquals(1964, fundCnt);
 
-            Assert.assertArrayEquals(expectedTypes, types.toArray());
-            Assert.assertArrayEquals(expectedNames, names.toArray());
+            final IntHolder cnt = new IntHolder();
+            JSONArray fundList = funds.getJSONArray("list");
+            fundList.forEach(o -> {
+                        //JSONObject fund = (JSONObject) o;
+                        //System.out.println(fund.getString("fundCode") + fund.getString("fundName"));
+                        cnt.value++;
+                    }
+            );
+            Assert.assertEquals(fundCnt, cnt.value);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testFundPage() {
+
+        InputStream in = JsoupTest.class.getClassLoader().getResourceAsStream("files\\jingzhi_sample_110025.html");
+        try {
+            Document doc = Jsoup.parse(in, CHARSET_NAME, DEFAULT_BLANK_BASE_URL);
+            Elements elements = doc.select("table[class=\"n_table m_table\"] > tbody > tr");
+            Assert.assertEquals(3, elements.size());
+
+            ArrayList<String> dates = new ArrayList<String>();
+            ArrayList<String> netValues = new ArrayList<String>();
+            ArrayList<String> AccNetValues = new ArrayList<String>();
+            ArrayList<String> dailyChangePcts = new ArrayList<String>();
+            for (Element element : elements) {
+                Elements record = element.select("td");
+                Assert.assertEquals(4, record.size());
+                dates.add(record.get(0).text());
+                netValues.add(record.get(1).text());
+                AccNetValues.add(record.get(2).text());
+                dailyChangePcts.add(record.get(3).text());
+            }
+
+            String[] expectedDates = {"2016-11-11", "2016-11-10", "2016-11-09"};
+            String[] expectedNetValues = {"1.0670", "1.0190", "1.0060"};
+            String[] expectedAccNetValues = {"1.0670", "1.0190", "1.0060"};
+            String[] expectedDailyChangePcts = {"4.71%", "1.29%", "1.41%"};
+
+            Assert.assertArrayEquals(expectedDates, dates.toArray());
+            Assert.assertArrayEquals(expectedNetValues, netValues.toArray());
+            Assert.assertArrayEquals(expectedAccNetValues, AccNetValues.toArray());
+            Assert.assertArrayEquals(expectedDailyChangePcts, dailyChangePcts.toArray());
 
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 
 
-    @Test
-    public void testEncoding() throws UnsupportedEncodingException {
-        String chinese = "中文";//java内部编码
-        String gbkChinese = new String(chinese.getBytes("GBK"),"ISO-8859-1");
-        String unicodeChinese = new String(gbkChinese.getBytes("ISO-8859-1"),"GBK");
-        System.out.println(unicodeChinese);//中文
-        String utf8Chinese = new String(unicodeChinese.getBytes("UTF-8"),"ISO-8859-1");
-        System.out.println(utf8Chinese);//乱码
-        unicodeChinese = new String(utf8Chinese.getBytes("ISO-8859-1"),"UTF-8");//java内部编码
-        System.out.println(unicodeChinese);//中文
-    }
+
+
+//    @Test
+//    public void testEncoding() throws UnsupportedEncodingException {
+//        String chinese = "中文";//java内部编码
+//        String gbkChinese = new String(chinese.getBytes("GBK"),"ISO-8859-1");
+//        String unicodeChinese = new String(gbkChinese.getBytes("ISO-8859-1"),"GBK");
+//        System.out.println(unicodeChinese);//中文
+//        String utf8Chinese = new String(unicodeChinese.getBytes("UTF-8"),"ISO-8859-1");
+//        System.out.println(utf8Chinese);//乱码
+//        unicodeChinese = new String(utf8Chinese.getBytes("ISO-8859-1"),"UTF-8");//java内部编码
+//        System.out.println(unicodeChinese);//中文
+//    }
 }
